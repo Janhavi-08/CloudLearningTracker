996518d6196f2d66beeef67ca552df4cb03add00
Janhavi <pjanhavi71@gmail.com>
2026-07-27 18:54:41 +0530

    Updated docker compose file

--- CloudLearningTracker.API/Program.cs
+++ CloudLearningTracker.API/Program.cs
@@ -46,9 +46,11 @@ builder.Services
                 new TokenValidationParameters
                 {
                     ValidateIssuer = true,
-                    ValidateAudience = true, 
+                    ValidateAudience = true,
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
                 };
     });
